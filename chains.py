69474e06c77973351f58bfc85b55c3c0d09a5a5f
Sufyan Arshad <sufyanarshad131@gmail.com>
2025-12-08 18:16:59 +0500

    Reviser Agent

--- chains.py
+++ chains.py
@@ -12,7 +12,7 @@ from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
 
-from schemas import AnswerQuestion 
+from schemas import AnswerQuestion, ReviseAnswer 
 
 llm = ChatOllama(model="llama3.2", temperature=0)
 parser = JsonOutputToolsParser(return_id=True)
@@ -43,7 +43,18 @@ first_responder = first_responder_prompt_template | llm.bind_tools(
     tools=[AnswerQuestion], tool_choice="AnswerQuestion"
 )
 
+revise_instructions = """Revise your previous answer using the new information.
+        - You should use the previous critique to add important information to your answer.
+        - You MUST include numerical citations in your revised answer to ensure it can be verified.
+        - Add a "References" section to the bottom of your answer (which does not count towards the word limit).
+            - [1] https://example.com
+            - [2] https://example.com
+        - You should use the previous critique to remove superfluous information from your answer and make SURE it is not more than 250 words.
+"""
 
+revisor = actor_prompt_template.partial(
+    first_instruction = revise_instructions
+) | llm.bind_tools(tools=[ReviseAnswer], tool_choice="ReviseAnswer")
 
 if __name__ == "__main__":
     human_message = HumanMessage(
